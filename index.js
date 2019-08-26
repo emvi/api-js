02d0f05003519e0577408fba6473d591704185c5
Marvin Blum <marvin@marvinblum.de>
2019-08-26 21:51:03 +0200

    Added request header configuration.

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const authenticationEndpoint = "/api/v1/auth/token";
 const searchArticlesEndpoint = "/api/v1/search/article";
 
 module.exports = class EmviClient {
-	constructor(client_id, client_secret, config) {
+	constructor(client_id, client_secret, organization, config) {
 		this.client_id = client_id;
 		this.client_secret = client_secret;
+		this.organization = organization;
 
 		if(typeof config !== "object") {
 			config = {};
@@ -61,7 +62,7 @@ module.exports = class EmviClient {
 		filter.query = query;
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+searchArticlesEndpoint, {params: filter})
+			axios.get(this.api_host+searchArticlesEndpoint, {params: filter}, this._config())
 			.then(r => {
 				let results = r.data.articles ? r.data.articles : [];
 				resolve({results, count: r.data.count});
@@ -70,5 +71,15 @@ module.exports = class EmviClient {
 				reject(e);
 			});
 		});
+	}
+
+	_config() {
+		return {
+			headers: {
+				"Authorization": `Bearer ${this.access_token}`,
+				"Organization": this.organization,
+				"Client": this.client_id
+			}
+		};
 	}
 };
