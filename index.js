766a2482b3cc64e5f6a1a44160f96736c23b9238
Marvin Blum <marvin@marvinblum.de>
2019-11-04 00:24:58 +0100

    Use axios instance instead of global configuration.

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ module.exports = class EmviClient {
 		this.token_type = window.localStorage.getItem("token_type");
         this.access_token = window.localStorage.getItem("access_token");
         this.expires_in = window.localStorage.getItem("expires_in");
+        this.axios = axios.create();
 		this._addAxiosInterceptor();
 	}
 
@@ -44,7 +45,7 @@ module.exports = class EmviClient {
                 client_secret: this.client_secret
 			};
 
-			axios.post(this.auth_host+authenticationEndpoint, req)
+			this.axios.post(this.auth_host+authenticationEndpoint, req)
 			.then(r => {
 				this.token_type = r.data.token_type;
                 this.access_token = r.data.access_token;
@@ -64,7 +65,7 @@ module.exports = class EmviClient {
 		filter = this._checkSearchParamsAndBuildFilter(query, filter);
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+searchArticlesEndpoint, {headers: this._config().headers, params: filter})
+			this.axios.get(this.api_host+searchArticlesEndpoint, {headers: this._config().headers, params: filter})
 			.then(r => {
 				r.data.articles = r.data.articles || [];
 				resolve(r.data);
@@ -76,7 +77,7 @@ module.exports = class EmviClient {
 		filter = this._checkSearchParamsAndBuildFilter(query, filter);
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+searchListsEndpoint, {headers: this._config().headers, params: filter})
+			this.axios.get(this.api_host+searchListsEndpoint, {headers: this._config().headers, params: filter})
 			.then(r => {
 				r.data.lists = r.data.lists || [];
 				resolve(r.data);
@@ -88,7 +89,7 @@ module.exports = class EmviClient {
 		filter = this._checkSearchParamsAndBuildFilter(query, filter);
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+searchTagsEndpoint, {headers: this._config().headers, params: filter})
+			this.axios.get(this.api_host+searchTagsEndpoint, {headers: this._config().headers, params: filter})
 			.then(r => {
 				r.data.tags = r.data.tags || [];
 				resolve(r.data);
@@ -113,7 +114,7 @@ module.exports = class EmviClient {
 		}
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+searchAllEndpoint, {headers: this._config().headers, params: filter})
+			this.axios.get(this.api_host+searchAllEndpoint, {headers: this._config().headers, params: filter})
 			.then(r => {
 				r.data.articles = r.data.articles || [];
 				r.data.groups = r.data.groups || [];
@@ -127,7 +128,7 @@ module.exports = class EmviClient {
 
 	getOrganization() {
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+organizationEndpoint, {headers: this._config().headers})
+			this.axios.get(this.api_host+organizationEndpoint, {headers: this._config().headers})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -136,7 +137,7 @@ module.exports = class EmviClient {
 
 	getLanguages() {
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+languagesEndpoint, {headers: this._config().headers})
+			this.axios.get(this.api_host+languagesEndpoint, {headers: this._config().headers})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -147,7 +148,7 @@ module.exports = class EmviClient {
 		this._checkParamIsString(id);
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+languageEndpoint.replace("{id}", id), {headers: this._config().headers})
+			this.axios.get(this.api_host+languageEndpoint.replace("{id}", id), {headers: this._config().headers})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -166,7 +167,7 @@ module.exports = class EmviClient {
 		}
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+articleEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId, version}})
+			this.axios.get(this.api_host+articleEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId, version}})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -185,7 +186,7 @@ module.exports = class EmviClient {
 		}
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+articleHistoryEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId, offset}})
+			this.axios.get(this.api_host+articleHistoryEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId, offset}})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -205,7 +206,7 @@ module.exports = class EmviClient {
 		}
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+pinnedEndpoint, {headers: this._config().headers, params: {articles, lists, offset_articles, offset_lists}})
+			this.axios.get(this.api_host+pinnedEndpoint, {headers: this._config().headers, params: {articles, lists, offset_articles, offset_lists}})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -220,7 +221,7 @@ module.exports = class EmviClient {
 		}
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+listEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId}})
+			this.axios.get(this.api_host+listEndpoint.replace("{id}", id), {headers: this._config().headers, params: {lang: langId}})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -231,7 +232,7 @@ module.exports = class EmviClient {
 		filter = this._checkListEntriesParamsAndBuildFilter(id, langId, filter);
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+listEntriesEndpoint.replace("{id}", id), {headers: this._config().headers, params: filter})
+			this.axios.get(this.api_host+listEntriesEndpoint.replace("{id}", id), {headers: this._config().headers, params: filter})
 			.then(r => {
 				r.data.entries = r.data.entries || [];
 				resolve(r.data);
@@ -243,7 +244,7 @@ module.exports = class EmviClient {
 		this._checkParamIsString(name, "name");
 
 		return new Promise((resolve, reject) => {
-			axios.get(this.api_host+tagEndpoint.replace("{name}", name), {headers: this._config().headers})
+			this.axios.get(this.api_host+tagEndpoint.replace("{name}", name), {headers: this._config().headers})
 			.then(r => {
 				resolve(r.data);
 			});
@@ -251,12 +252,12 @@ module.exports = class EmviClient {
 	}
 
 	_addAxiosInterceptor() {
-		axios.interceptors.response.use(null, e => {
+		this.axios.interceptors.response.use(null, e => {
 			if(e.config && e.response && e.response.status === 401) {
 				return this.refreshToken()
 				.then(() => {
 					e.config.headers = this._config().headers;
-					return axios.request(e.config);
+					return this.axios.request(e.config);
 				});
 			}
 
